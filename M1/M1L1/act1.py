e7e05dd371e63856910821e9e06e6f0adc9056fe
coderlohitaksh <coderlohitaksh@gmail.com>
2025-05-31 17:16:46 +0530

    done

--- M1/M1L1/act1.py
+++ M1/M1L1/act1.py
@@ -1,3 +1,11 @@
-#print lines
+#printing a greeting messege
 print("Hello world")
 
+#printing my name
+print("my name is Liyan")
+
+#printing my hobby
+print("my hobby is playing chess")
+
+#printing a simple calculation
+print("the answer of (3 + 5 - 2) is :", (3 + 5 - 2) )
