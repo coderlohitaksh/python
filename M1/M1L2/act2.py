2c659f064b5a0caf327a8ff36d01e8b5d38c8108
coderlohitaksh <coderlohitaksh@gmail.com>
2025-05-31 18:27:42 +0530

    oo

---
+++ M1/M1L2/act2.py
@@ -0,0 +1,14 @@
+#Creating different variables
+
+name = input ("Enter your name : ")
+x = " Hello "
+y = name
+
+print(x + y)
+
+z = " Welcome to python" 
+print(z)
+
+#input name
+
+print("\n Hello" , name , "welcome to codingal")
