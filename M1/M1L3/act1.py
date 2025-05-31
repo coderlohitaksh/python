1c8e5eec5fd6d2534ed647b5a0682c8ec79fb0c7
coderlohitaksh <coderlohitaksh@gmail.com>
2025-05-31 20:02:10 +0530

    done

---
+++ M1/M1L3/act1.py
@@ -0,0 +1,16 @@
+#Let's check the datatype of different values
+
+A = 52
+print("type of A is : ", type(A))
+
+B = 52.52
+print("type of B is : ", type(B))
+
+C = " qwerty "
+print("type of C is : ", type(C))
+
+D = True
+print("type of D is : ", type(D))
+
+print ("The end")
+s
