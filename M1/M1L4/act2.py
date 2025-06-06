5f79d989d3b05d7f652aa37181d2a1cf738f707b
coderlohitaksh <coderlohitaksh@gmail.com>
2025-06-06 20:53:27 +0530

    done

---
+++ M1/M1L4/act2.py
@@ -0,0 +1,15 @@
+#Write a program to calculate the number of notes in the given amount?
+
+#Total as input
+amount = int(input("Please enter your amount for withdraw"))
+
+#Calculating the number of notes
+
+n1 = amount//100
+n2 = (amount%100)//50
+n3 = ((amount%100)%50)//10
+
+
+print ("note of 100 rupees is : " , n1)
+print ("note of 50 rupees is : " , n2)
+print ("note of 10 rupees is : " , n3)
