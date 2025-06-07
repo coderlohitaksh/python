ed17b97dfaf83b0a29a7e344fa266c29bc90b37f
coderlohitaksh <coderlohitaksh@gmail.com>
2025-06-07 21:44:13 +0530

    done

---
+++ M1/M1L4/project.py
@@ -0,0 +1,6 @@
+#This project aims to understand how to apply a formula to find the square root of the given number.
+
+n = int(input("enter your number to square root : "))
+dividend = n ** 0.5
+
+print(dividend)
