862615f24bc3c3252cb4c06d14bd9794366c382d
coderlohitaksh <coderlohitaksh@gmail.com>
2025-06-13 20:42:01 +0530

    done

---
+++ M1/M1L5/act1.py
@@ -0,0 +1,11 @@
+#write a program to check whether the given number is positive?
+
+num = 3
+if num > 0:
+    print (num, "is a positive number")
+
+num = -1
+if num < 0:
+    print (num, "is a negative number")
+
+
