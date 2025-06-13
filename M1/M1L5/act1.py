d301760611ef2fc496fb9697510725a722153eca
coderlohitaksh <coderlohitaksh@gmail.com>
2025-06-13 21:13:09 +0530

    ;'.'

---
+++ M1/M1L5/act1.py
@@ -0,0 +1,9 @@
+#write a program to check whether the give number is positive or negative
+
+num = 34
+if num > 0:
+    print (num , "is a positive number .")
+
+num = -34
+if num < 0:
+    print (num , "is a negative number .")
