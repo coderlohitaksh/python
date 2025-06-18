991be39d20547fa82f0525d9f2ac552feff3e19c
coderlohitaksh <coderlohitaksh@gmail.com>
2025-06-18 17:33:04 +0530

    done

---
+++ M1/M1L5/project.py
@@ -0,0 +1,7 @@
+temp = float(input("Enter your tempature in C° : "))
+if (temp >= 30):
+     print("It's warm enough. You can wear light clothes!")
+elif temp >= 15:
+    print("It's a bit cool. You can wear light clothes with a light jacket.")
+else:
+    print("It's too cold. Better wear warm clothes like a jacket or pullover.")
