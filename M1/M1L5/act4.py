4a4346f7002f11ba1c9c898673ca321054ab9bd3
coderlohitaksh <coderlohitaksh@gmail.com>
2025-06-18 16:01:24 +0530

    done

---
+++ M1/M1L5/act4.py
@@ -0,0 +1,10 @@
+#Write a program to check the number is odd or even.
+
+print("Let us check if your number is odd or even!!!")
+
+num = int(input("Enter your number : "))
+
+if (num%2 == 0):
+    print("Your number is even")
+else:
+    print("Your number is odd")
