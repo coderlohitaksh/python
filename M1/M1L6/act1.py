12fbaba037f8f0f50002aec2cbc90666673722ad
coderlohitaksh <coderlohitaksh@gmail.com>
2025-06-20 20:59:52 +0530

    done

---
+++ M1/M1L6/act1.py
@@ -0,0 +1,11 @@
+# Checking booleans
+
+a = 25
+b = 98
+c = 88
+
+if a > b and a < c:
+    print("I am using 'and' operator")
+elif a < b or a > c:   
+    print("I am using 'or' operator") 
+    
---
+++ M1/M1L6/act2.py
@@ -0,0 +1,25 @@
+# 1st
+
+a = 15
+b = 12
+c = 13
+
+print(a != b)
+print(b != c)
+
+# 2nd
+
+a = "python"
+b = "coding"
+
+if a != b:
+    print(a," and ",b," are not the same .")
+
+# 3rd
+
+a = 4
+b = 5
+
+if (a == 4) != (b == 5):
+    print(a," and ",b," are not the same .")
+
