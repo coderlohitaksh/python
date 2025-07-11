d3d3c882f37105e8672e0823cd9856966d209d0c
coderlohitaksh <coderlohitaksh@gmail.com>
2025-07-11 21:04:04 +0530

    done

---
+++ M1/M1L8/act2.py
@@ -0,0 +1,9 @@
+#2nd
+
+n1 = int(input("Enter you numerator : "))
+n2 = int(input("Enter your demoninator : "))
+
+if (n1 % n2 == 0):
+    print("The number",n1 ,"is divisible by the",n2 ,".")
+else:
+    print("The number ",n1 ,"is not divisble by ",n2 ,".")
