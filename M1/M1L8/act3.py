a172adde882cb3d09b60ec2b0a59567075db4680
coderlohitaksh <coderlohitaksh@gmail.com>
2025-07-11 20:45:30 +0530

    done

---
+++ M1/M1L8/act3.py
@@ -0,0 +1,15 @@
+
+
+m1 = 22
+wn = 34
+rn = 45
+res = 40
+
+sum = m1 + res
+print("The sum of the total numbers is ",sum," .")
+
+sum2 = sum - (wn + rn)
+print("The corrected sum is ",sum2, ".")
+
+m2 = sum/res
+print(m2)
