2d5b9fe4a1908ee2224f8f5753cbd1ed799f8abd
coderlohitaksh <coderlohitaksh@gmail.com>
2025-08-01 21:00:04 +0530

    done

--- M2/M2L3/act3.py
+++ M2/M2L3/act3.py
@@ -1,2 +1,16 @@
 # Amstrong number
 
+print("\nHello !! welcome to my project , it is called an amstrong calculator .")
+print("A number that is equal to the sum of its own digits each raised to the power of the number of digits")
+num = int(input("Enter your number to be amstronged : "))
+sum = 0
+temp = num
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** 3
+    temp // 3
+
+if num == sum :
+    print("\nThe number ",num," is an amstrong number . ")
+else :
+    print("\nThe number ",num," is an not amstrong number . ")
