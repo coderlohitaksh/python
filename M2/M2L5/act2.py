8520ec4d7363693a4bf7a5c0bf153caf45435ffa
coderlohitaksh <coderlohitaksh@gmail.com>
2025-08-15 20:49:30 +0530

    done

---
+++ M2/M2L5/act2.py
@@ -0,0 +1,13 @@
+#Floyd's triangle
+#Floyd's triangle is a right - angle triangle
+
+rows = int(input("Enter the number the total number of rows : "))
+number = 1
+print("Presenting : Floyd's Triangle ")
+
+for i in range (1 , rows + 1):
+    for j in range (1 , i + 1):
+        print(number , end = "")
+        number - number + 1
+        print()
+
