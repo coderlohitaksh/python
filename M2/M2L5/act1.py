d5321b300545949a3d408f16962bad74b1262196
coderlohitaksh <coderlohitaksh@gmail.com>
2025-08-16 17:05:15 +0530

    done

--- M2/M2L5/act1.py
+++ M2/M2L5/act1.py
@@ -1,12 +1,9 @@
 print("Half Pyramid Pattern of Stars (*):")
 
-n = int(input("enter the number of rows: "))
-
-#outer loop to handle number of rows
+n = int(input("Enter the number of rows: "))
 
 for i in range(n):
-    #inner loop to handle number of columns
+
     for j in range(i + 1):
         print("* ", end="")
-
-print()
+    print()  
--- M2/M2L5/act2.py
+++ M2/M2L5/act2.py
@@ -9,5 +9,5 @@ for i in range (1 , rows + 1):
     for j in range (1 , i + 1):
         print(number , end = "")
         number - number + 1
-        print()
+    print()
 
