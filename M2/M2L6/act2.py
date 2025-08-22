81a43147ab0ca5388d3964701c3abb90ed544a37
coderlohitaksh <coderlohitaksh@gmail.com>
2025-08-22 20:42:42 +0530

    done

---
+++ M2/M2L6/act2.py
@@ -0,0 +1,16 @@
+# POLYGON 
+import turtle
+
+turtle.Screen().bgcolor("orange")
+turtle.Screen().setup(300 , 400)
+polygon = turtle.Turtle()
+
+sides = 6
+side_length = 70
+angle = 360.0/sides
+
+for i in range (sides):
+    polygon.forward (side_length)
+    polygon.right (angle)
+    
+    turtle.done()
