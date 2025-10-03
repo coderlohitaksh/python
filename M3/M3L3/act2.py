51ca5adc0afa268fdea8228c88103eb056910be1
coderlohitaksh <coderlohitaksh@gmail.com>
2025-10-03 20:40:32 +0530

    d

---
+++ M3/M3L3/act2.py
@@ -0,0 +1,11 @@
+for x in range(10):
+    if x % 20 == 0 :
+        print("Twist🎗️")
+    elif x % 15 == 0 :
+        pass
+    elif x % 5 == 0 :
+        print("Fizz🥤")
+    elif x % 3 == 0 :
+        print("Buzz")
+    else:
+        print(x)
