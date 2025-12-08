97b92e735a5e7a329fef54d5ac152d6eb22f80e3
coderlohitaksh <coderlohitaksh@gmail.com>
2025-12-08 21:19:54 +0530

    d

---
+++ M4/M4L1/act2.py
@@ -0,0 +1,14 @@
+def match_words(words):
+    ctr = 0
+    lst = []
+    for words in words :
+        if len(words) > 1 and words[0] == words[-1]:
+            ctr += 1
+            lst.append(words)
+
+    print("List of words having first and last character same : \n",lst)
+    return ctr
+count = match_words(['abc', 'cfc','xyz', 'aba', '1221'])
+
+print("Number of words having first and last character same:", count)
+
