3357c5c35fcacb69fd02cd7d27a93473150fe180
coderlohitaksh <coderlohitaksh@gmail.com>
2025-12-13 17:33:05 +0530

    d

--- M4/M4L2/act3.py
+++ M4/M4L2/act3.py
@@ -1,1 +1,33 @@
-weather = ("")
+from colorama import Fore, init
+init(autoreset=True)
+sunny = 0
+rainy = 0
+weather = []
+
+print("Enter weather for 7 days (1 for sunny, 0 for rainy):")
+for i in range(7):
+    while True:
+        try:
+            day = int(input(f"Day {i+1}: "))
+            if day not in (0, 1):
+                print(Fore.YELLOW + "Please enter 0 for rainy or 1 for sunny.")
+                continue
+            weather.append(day)
+            break
+        except ValueError:
+            print(Fore.YELLOW + "Invalid input. Enter 0 or 1.")
+
+print("\nWeather for the week:")
+for i, day in enumerate(weather):
+    if day == 1:
+        print(f"Day {i+1}: " + Fore.YELLOW + "Sunny☀️")
+        sunny += 1
+    else:
+        print(f"Day {i+1}: " + Fore.BLUE + "Rainy🌦️")
+        rainy += 1
+
+print("\nOverall weather:")
+if sunny > rainy:
+    print(Fore.GREEN + "Good weather")
+else:
+    print(Fore.RED + "Bad weather")
