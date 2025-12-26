798062e4cc1aef11b0824ea9a40c0ac82ae00344
coderlohitaksh <coderlohitaksh@gmail.com>
2025-12-26 20:53:38 +0530

    done

---
+++ M4/M4L4/act2.py
@@ -0,0 +1,6 @@
+set1 = {'green' , 'blue'}
+set2 = {'blue' , 'yellow'}
+print("Orginal sets were ",set1 , set2)
+print("The intersection of both sets equal to ")
+set3 = set1.intersection(set2)
+print(set3)
